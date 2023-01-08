79f3f6b8efc177a0be51299fcd5295841b2bc408
Gianncarlo Ciampaglia <giank7@gmail.com>
2023-01-08 23:08:36 +0000

    ADD instructions function

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import sys
 ROWS = 5
 COLUMNS = 5
 SEA = " "
+line_break = "\n" + str("-" * 80) + "\n"
 
 def print_slow(str):
     for char in str:
@@ -14,7 +15,7 @@ def print_slow(str):
         time.sleep(.005)
 
 def title():
-    print("<====>  BATTLESHIP: Guadalcanal 1942  <====>\n")
+    print("<====>  BATTLESHIP: Guadalcanal 1942  <====>")
 
 def introduction_history_mode():
     """
@@ -22,7 +23,7 @@ def introduction_history_mode():
     """
 
     intro_history_mode = [
-        "\nOn the night of November 14th 1942 Admiral William Halsey " +
+        "On the night of November 14th 1942 Admiral William Halsey " +
         "took the command of a task force \nformed of battleships USS Washington, " +
         "USS South Dakota and 4 destroyers. The mission was to \nprotect Henderson " +
         "field from the Japanese Naval forces.\n"
@@ -33,8 +34,10 @@ def introduction_history_mode():
         "2 Heavy Cruisers, that occupy 2 cells\n"
         "4 Destroyers that occupy 1 cell each\n"
     ]
+    print(line_break)
     for i in intro_history_mode:
         print_slow(i)
+    print(line_break)
     continue_key()
 
 def clear_screen():
@@ -44,11 +47,32 @@ def clear_screen():
         os.system("clr")
 
 def continue_key():
-    input("\nPress Enter to continue...")
-    clear_screen()
+    input("Press Enter to continue...")
+    
 
 def instructions_history_mode():
+    """
+    Intructions to play the game
+    """
+
+    instructions = [
+        "You must input two coordinates in order to select the" +
+        "grid you want to fire upon:\n" +
+        "\n1. Select the grid ROW by choosing a NUMBER" +
+        "\n2. Select the grid COLUMN by choosing a LETTER\n" +
+        "\n* If a ship is within the grid coordinates you have selected, " +
+        "a hit will be \nregistered.",
+        "\n* If the grid is empty, it will be registered as a miss",
+        "\n* The size of the grid and the amount of shells you have " +
+        "will be determined by \n  the difficulty level you choose"
+    ]
+    clear_screen()
+    title()
+    print(line_break)
+    for i in instructions:
+        print_slow(i)
     print("instructions")
+    print(line_break)
     continue_key()
 """
 def get_initial_battlefield():
