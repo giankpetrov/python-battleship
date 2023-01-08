54bed63f5871a39fbebf4824a9155d5d5325b684
Gianncarlo Ciampaglia <giank7@gmail.com>
2023-01-08 00:20:04 +0000

    ADD def initial battlefield and its variables

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+import random
+
+ROWS = 5
+COLUMNS = 5
+SEA = " "
+
+def get_initial_battlefield():
+    battlefield = []
+    for y in range(ROWS):
+        battlefield.append([])
+        for x in range(COLUMNS):
+            battlefield[y].append(SEA)
+    return battlefield
